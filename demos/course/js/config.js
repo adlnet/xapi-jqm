dbf6cbc200c021b0f3a7789693263ce91aa746b9
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-06-02 15:20:54 -0400

    display checkboxes and text value

--- demos/course/js/config.js
+++ demos/course/js/config.js
@@ -9,9 +9,13 @@ var email = "tyler.mulligan.ctr@adlnet.gov";
 var username = "French Toaster";
 var actor = { "mbox": "mailto:" + email, "name": username };
 */
-Config.endpoint = "https://lrs.adlnet.gov/xapi/";
-Config.user = "jqm";
-Config.password = "xapijqm";
+// Config.endpoint = "https://lrs.adlnet.gov/xapi/";
+// Config.user = "jqm";
+// Config.password = "xapijqm";
+Config.endpoint = "http://localhost:8000/xapi/";
+Config.user = "lou";
+Config.password = "password";
+
 //Config.actor = actor;
 
 // "global" variables
@@ -19,3 +23,6 @@ var courseID = "http://adlnet.gov/xapi/samples/xapi-jqm";
 var courseType = "http://adlnet.gov/xapi/activities/course";
 
 var linkType = "http://adlnet.gov/xapi/activities/link";
+var quizType = "http://adlnet.gov/xapi/activities/quiz";
+
+var CORRECT_QUIZ_ANSWERS =[[2,3,6], [4], "toast"];
