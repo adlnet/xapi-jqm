036875139954d61ab512bb448aa36c31142e432f
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-06-12 14:30:30 -0400

    added custom verbs namespace

---
+++ demos/course/js/verbs.js
@@ -0,0 +1,14 @@
+(function(ADL){
+   ADL.custom = {
+      "verbs": {
+         "read" : {
+            "id" : "http://example.com/xapi/verbs/read",
+            "display" : {"en-US" : "read"}
+         },
+         "watched" : {
+            "id" : "http://example.com/xapi/verbs/watched",
+            "display" : {"en-US" : "watched"}
+         }  
+      }   
+   };
+}(window.ADL = window.ADL || {}));
