3278a8ec898190d6446bebb52f7c4df8c8eaaef8
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-06-13 10:00:53 -0400

    added checked and unchecked verbs and implemented. scrubbed contextActivities

--- demos/course/js/verbs.js
+++ demos/course/js/verbs.js
@@ -5,10 +5,14 @@
             "id" : "http://example.com/xapi/verbs/read",
             "display" : {"en-US" : "read"}
          },
-         "watched" : {
-            "id" : "http://example.com/xapi/verbs/watched",
-            "display" : {"en-US" : "watched"}
-         }  
+         "checked" : {
+            "id" : "http://example.com/xapi/verbs/checked",
+            "display" : {"en-US" : "checked"}
+         },
+         "unchecked" : {
+            "id" : "http://example.com/xapi/verbs/unchecked",
+            "display" : {"en-US" : "unchecked"}
+         }           
       }   
    };
 }(window.ADL = window.ADL || {}));
--- demos/course/js/video-functions.js
+++ demos/course/js/video-functions.js
@@ -2,9 +2,9 @@ var TUTORIAL_VIDEO_URL = "http://www.youtube-nocookie.com/embed/vPrtNzvDS5M?rel=
 
 // initialize youtube video tracking
 var chapterID = $("body").attr("data-chapter");
-var pageID = 1; // lazy
+var pageID = "p1"; // lazy
 var wrapper = Popcorn.HTMLYouTubeVideoElement("#How-to-make-french-toast-xapi-jqm-video");
 wrapper.src = TUTORIAL_VIDEO_URL 
 var video = Popcorn(wrapper);
-var videoContext = createContext(chapterID, pageID);
-ADL.XAPIVideo.addVideo(video, "", true, true, false, false, videoContext);
+var videoContext = createContext(chapterID, pageID, undefined, true);
+ADL.XAPIVideo.addVideo(video, "", true, false, false, false, videoContext);
